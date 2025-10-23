6252b366cd128ec5142f64b199f9bb35c7262a59
Benjamin <be.retamalr@duocuc.cl>
2025-10-23 13:51:30 -0300

    Actualiza dependencias de Babel a la versión 7.28.5 y elimina el enlace en el componente Logo

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom';
  
 export default function Logo({ className = "" }) { // Recibe header-logo
   return (
-    <Link to="/">
       <Image
         src="src/assets/logo.png"
         alt="Logo Huerto Hogar"
         className={className} // Aplica la clase 'header-logo' pasada desde Header
         id={className === 'logo-huerta' ? 'Logo-huerta' : undefined}
       />
-    </Link>
   );
 }
